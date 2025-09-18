4e79f9655bf42cf28748bec8f983898a2690c346
Sambasivam Krishnamurthy <smbkrishnamurthy@gmail.com>
2025-09-18 12:33:29 +0530

    comments updated

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	//Exercise 1.1
+	//Exercise 1.1 basic anonymous fn direct call
 	func(x, y int) {
 		fmt.Println(x + y)
 	}(2, 3)
 
-	//Exercise 1.2
+	//Exercise 1.2 basic anonymous fn in variable
 	square := func(x int) int {
 		return x * x
 	}
 	fmt.Println(square(4))
+
+	//Exercise 1.3 basic vlosure
+
 }
